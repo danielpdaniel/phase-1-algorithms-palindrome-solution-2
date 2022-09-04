7ca92ec46c631693fc2248f2eda5f54ab359fdaa
danielpdaniel <artofjquinn@gmail.com>
2022-09-03 20:16:13 -0400

    commit tree 96574a597045b310a5cef361d2904dfa187feabb

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i=0; i<word.length / 2; i++){
+    const j = word.length - 1 - i;
+    if(word[i] !== word[j]){
+      return false
+    }
+    return true
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  function isPalindrome(word){
+    for(let i=0; i<word.length; i++){
+      const j = word.length - 1 - i;
+      if(word[i] !== j){
+        return false
+      }
+      return true
+    }
+  }
 */
 
 /*
   Add written explanation of your solution here
+
+  will loop through the string to the middle from the beginning
+  and from the end while comparing each letter at beginning and end 
+  of the string, returning false if any arent equal to each other
 */
 
 // You can run `node index.js` to view these console logs
